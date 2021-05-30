d31f7cfbc38f03399fb3fd9227565661ef736347
Antonio J Rossi <antoniojrossi@gmail.com>
2021-05-30 15:16:21 +0200

    Code as created at end of lecture 2

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Antonio J Rossi on 30/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
